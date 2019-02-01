ccbaeb6d2df5298a1fb6e69e277eba3953ee9295
tinyafuro <tinyforest.win@gmail.com>
2019-02-02 00:47:21 +0900

    Implement bbasic login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  # セッション用ヘルパーモジュールを読み込む
+  include SessionsHelper
 
   def hello
     render html: "hello, wold!"
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,13 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
 
-  get 'signup', to: 'users#new'
-  post 'signup', to: 'users#create'
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+
+  # get 'sessions/new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
   resources :users
   
